8e1b5e09d85e9c6486e5a8acc902982e4615514c
swarajp891 <swaraj.pal@konverge.ai>
2023-12-13 01:11:57 +0530

    requirement file

--- CarProject/FlaskModel/lecun_api.py
+++ CarProject/FlaskModel/lecun_api.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,jsonify, request
 from flask_cors import CORS
+from waitress import serve
 app = Flask(__name__)
 CORS(app)
 from model import knn
@@ -26,4 +27,5 @@ def input_data():
 
 
 if __name__ == '__main__':
-    app.run(debug = True, port = 5001)
+    #app.run(host='0.0.0.0')
+    serve(app, host='0.0.0.0', port=5000, url_scheme='https')
